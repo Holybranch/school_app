823571f9e233db4287c24da920c0b3e74ac8d81a
Holybranch <joonhyuck1224@gmail.com>
2026-03-07 01:09:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // 항상 dist 폴더로 빌드
+    outDir: 'dist',   // Firebase에 배포되는 폴더
+    emptyOutDir: true // 빌드 전에 dist 폴더 비우기
   }
 })
