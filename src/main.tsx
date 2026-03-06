ec24c2d46ccc05020e715d5f6adb307a02be5125
Holybranch <joonhyuck1224@gmail.com>
2026-03-07 02:03:31 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // <--- 여기서 App을 제대로 불러오는지 확인!
-import './index.css'
+import App from './App'  // 여기서 우리가 수정한 App.tsx를 정확히 불러와야 합니다.
+import './index.css'     // 만약 index.css가 없어서 에러 나면 이 줄만 지우세요.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
